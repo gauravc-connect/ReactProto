1157e7f482b9c80b22ed3461663437fff51c3183
Gaurav Chakravorty <gchakravorty@synamedia.com>
2025-01-30 21:32:17 +0530

    Added parcel

--- App.js
+++ App.js
@@ -1,3 +1,9 @@
+import React from "react";
+//import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client"; 
+
+
 const heading = React.createElement("h1", {id : "heading", xyz : "abc"}, "Hello from my React !!!");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
+
